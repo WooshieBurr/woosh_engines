47f026b2232905c8d6a01e4e0ad1cb1d38994b6b
Wooshie <55280951+WooshieBurr@users.noreply.github.com>
2022-10-18 23:31:31 +0100

    Initial commit

---
+++ woosh_engines/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version "cerulean"
+
+games { "gta5" }
+
+name "woosh_engines"
+description "Temporary fun engine modifying tool."
+
+client_scripts {
+    "core/client/*.lua",
+}
+
+server_scripts {
+    "core/server/sv_main.lua",
+}
